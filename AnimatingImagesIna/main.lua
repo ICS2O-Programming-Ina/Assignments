ca9c2ded4c101d74581fa3f5e0397b229e37d2c2
Ina Tolo <ina.tolo@ocsbstudent.ca>
2020-03-08 23:17:39 -0400

    Added background for Animating Images

--- AnimatingImagesIna/main.lua
+++ AnimatingImagesIna/main.lua
@@ -1,7 +1,22 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: Animating Images 
+-- Name: Ina 
+-- Course: ICS2O 
+-- This program displays a sunset with a palm tree. It shows a cloud and a sun moving. 
 
--- Your code here
+-- hide status bar 
+display.setStatusBar(display.HiddenStatusBar)
+
+-- background image with width and height 
+local backgroundImage = display.newImageRect("Images/city.jpg", 2045 , 1536)
+backgroundImage:scale( 1, 1)
+
+-- local character image with width and height 
+local sun = display.newImageRect("Images/sun.png", 200, 200)
+
+-- local character image with a width and height 
+local cloud = display.newImageRect("Images/clouds.png", 150, 150)
+
+-- set the image to be transparent 
+sun.alpha = 0 
+
+-- set the initial x and y 
