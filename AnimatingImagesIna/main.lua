984cde69f3d675ee0b5a5252c4b2c99ad091c633
Ina Tolo <ina.tolo@ocsbstudent.ca>
2020-03-09 14:32:43 -0400

    added to animating images

--- AnimatingImagesIna/main.lua
+++ AnimatingImagesIna/main.lua
@@ -7,16 +7,34 @@
 display.setStatusBar(display.HiddenStatusBar)
 
 -- background image with width and height 
-local backgroundImage = display.newImageRect("Images/city.jpg", 2045 , 1536)
-backgroundImage:scale( 1, 1)
+local backgroundImage = display.newImageRect("Images/vancouver.jpeg", 1024, 768)
+backgroundImage.x = display.contentCenterX
+backgroundImage.y = display.contentCenterY
+
 
 -- local character image with width and height 
-local sun = display.newImageRect("Images/sun.png", 200, 200)
+local sun = display.newImageRect("Images/sun.png", 125, 125)
 
 -- local character image with a width and height 
-local cloud = display.newImageRect("Images/clouds.png", 150, 150)
+local clouds = display.newImageRect("Images/clouds.png", 350, 350)
+
+-- local character image with a width and height 
+local palmTree = display.newImageRect("Images/palmTree.png", 900, 900)
 
 -- set the image to be transparent 
-sun.alpha = 0 
+sun.alpha = .8
 
--- set the initial x and y 
+-- set the initial x and y posititon of the sun
+sun.x = 155
+sun.y = 75
+
+-- set the initial x and y position of the clouds
+clouds.x = 160
+clouds.y = 75
+
+-- set the image to be transparent
+palmTree.alpha = .9
+
+-- set the initial x and y position of the palm tree 
+palmTree.x = 512
+palmTree.y = 760
