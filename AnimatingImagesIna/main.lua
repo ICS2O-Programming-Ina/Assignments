33a9481d5b58203b46419e10d131814262a0a03a
Ina Tolo <ina.tolo@ocsbstudent.ca>
2020-03-09 21:26:05 -0400

    added spinning to Animating Images

--- AnimatingImagesIna/main.lua
+++ AnimatingImagesIna/main.lua
@@ -6,14 +6,28 @@
 -- hide status bar 
 display.setStatusBar(display.HiddenStatusBar)
 
+-- global variables 
+scrollSpeed = 2
+
 -- background image with width and height 
 local backgroundImage = display.newImageRect("Images/vancouver.jpeg", 1024, 768)
 backgroundImage.x = display.contentCenterX
 backgroundImage.y = display.contentCenterY
 
-
 -- local character image with width and height 
 local sun = display.newImageRect("Images/sun.png", 125, 125)
+
+local function rockSun()
+	if ( reverse == 0 ) then 
+		reverse = 1
+		transition.to( sun, { rotation = -45, time = 500, transition = easing.inOutCubic } )
+	else
+		reverse = 0
+		transition.to ( sun, { rotation = 45, time = 500, transition = easing.inOutCubic } )
+	end
+end
+
+timer.performWithDelay( 600, rockSun, 0 ) -- Repeat forever 
 
 -- local character image with a width and height 
 local clouds = display.newImageRect("Images/clouds.png", 350, 350)
@@ -21,8 +35,11 @@ local clouds = display.newImageRect("Images/clouds.png", 350, 350)
 -- local character image with a width and height 
 local palmTree = display.newImageRect("Images/palmTree.png", 900, 900)
 
+-- local character image with a width and height 
+local sparkle = display.newImageRect("Images/sparkle.png", 600, 550)
+
 -- set the image to be transparent 
-sun.alpha = .8
+sun.alpha = 0
 
 -- set the initial x and y posititon of the sun
 sun.x = 155
@@ -33,8 +50,50 @@ clouds.x = 160
 clouds.y = 75
 
 -- set the image to be transparent
-palmTree.alpha = .9
+clouds.alpha = 1
 
 -- set the initial x and y position of the palm tree 
 palmTree.x = 512
 palmTree.y = 760
+
+-- set the initial x and y position of the sparkle
+sparkle.x = 512
+sparkle.y = 375
+
+local xScale = ball.width
+local yScale = ball.height 
+
+local function sunIncrease()
+	transition.to( ball, { time = 500, delay = 500, w = 1, h = 1})
+end 
+
+
+
+-- Function: MoveSun 
+-- Input: this function accepts an event listener 
+-- Output: none
+-- Description: This function add the scroll speed to the x-value of the sun 
+local function MoveSun(event)
+	-- add the scroll speed to the x-value of the sun 
+	sun.x = sun.x + scrollSpeed 
+	-- change the transparency of the sun every time it moves so that it fades out 
+	sun.alpha = sun.alpha + 0.1
+end
+
+-- MoveSun will be called over and over again 
+Runtime:addEventListener("enterFrame", MoveSun)
+
+
+-- Function: MoveClouds
+-- Input: this function accepts an event listener 
+-- Output: none
+-- Description: This function add the scroll speed to the x-value of the clouds
+local function MoveClouds(event)
+	-- add the scroll speed to the x-value of the clouds
+	clouds.x = clouds.x + scrollSpeed 
+	-- change the transparency of the clouds every time it moves so that it fades out 
+	clouds.alpha = clouds.alpha - .001
+end
+
+-- MoveClouds will be called over and over again 
+Runtime:addEventListener("enterFrame", MoveClouds)
